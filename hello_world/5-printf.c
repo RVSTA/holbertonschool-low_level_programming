bfee6dc1d31b206cbb43ee5f87577950a7d6705c
RVSTA <leorods12@outlook.com>
2025-02-18 11:39:12 -0400

    Printing

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string to both console and files.
+ *
+ * Return: value 0.
+ */
+
+int main(void)
+{
+	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
