b212bb63b866bb2a865bb0c5ee966d6495a6169c
RVSTA <leorods12@outlook.com>
2025-02-20 13:38:09 -0400

    alhpabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - Prints alphabet in lowercase + new line
+ *
+ *
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar("\n");
+}
