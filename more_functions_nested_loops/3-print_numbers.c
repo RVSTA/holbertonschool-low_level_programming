0e6360d98ffa3a90f70b6182b28c0cb483f0268d
RVSTA <leorods12@outlook.com>
2025-02-25 10:52:18 -0400

    numberz

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_numbers - Prints numbers form 0 to 9
+ *Return: no return
+ */
+void print_numbers(void)
+{
+	int number = 48;
+	for (number = 48; number < 58; number++)
+	{
+		_putchar(number);
+	}
+	_putchar(10);
+}
