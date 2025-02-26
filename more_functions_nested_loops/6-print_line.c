fec34327c60f019d45185768bd262e66b42b6f3b
RVSTA <leorods12@outlook.com>
2025-02-26 14:00:28 -0400

    lines lines lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_line - Draws straight line
+ *@n: is a parameter
+ *Return: None
+ */
+void print_line(int n)
+{
+	while (n-- > 0)
+		_putchar('_');
+	_putchar('\n');
+}
