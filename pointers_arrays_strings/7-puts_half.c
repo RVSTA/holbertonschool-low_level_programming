2f280b03297ff1776d8a6acf7f94bc841e16a7d3
RVSTA <leorods12@outlook.com>
2025-03-09 22:46:39 -0400

    fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,28 +1,34 @@
 #include "main.h"
 
 /**
- *puts_half - function that prints half of a string, followed by a new line.
- *
- *@str: This is the input string
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
  */
 void puts_half(char *str)
 
 {
-	int swagg, half;
+	int len, n, i;
 
-	swagg = 0;
-	while (str[swagg] != '\0')
-		swagg++;
+	len = 0;
 
-	half = swagg / 2;
-
-	if (swagg % 2 == 1)
-		half++;
-
-	while (half < swagg)
+	while (str[len] != '\0')
 	{
-		_putchar(str[swagg]);
-		half++;
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
 	}
 	_putchar('\n');
 }
